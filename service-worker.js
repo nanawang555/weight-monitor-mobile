2a141a72c934c57d2fa83678d5f2b7050baca8e9
nanawang555 <yuelinw.wu@gmail.com>
2026-09-10 00:04:52 +0800

    修复手机缓存更新

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const cacheName = 'weight-monitor-v4';
+const cacheName = 'weight-monitor-v5';
 const appFiles = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
-self.addEventListener('install', (event) => event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appFiles))));
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+self.addEventListener('install', (event) => event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appFiles)).then(() => self.skipWaiting())));
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', (event) => { if (event.request.mode === 'navigate') { event.respondWith(fetch(event.request).catch(() => caches.match('./'))); return; } event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))); });
